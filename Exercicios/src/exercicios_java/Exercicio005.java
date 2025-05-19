c4cbc44ec56bf23f2a3898617a1d6bc201f95873
samueljesus <samueljesus@pop-os>
2025-05-19 16:00:51 -0300

    Teste, estudo git

--- Exercicios/src/exercicios_java/Exercicio005.java
+++ Exercicios/src/exercicios_java/Exercicio005.java
@@ -1,5 +1,28 @@
 package exercicios_java;
 
-public class Exercicio005 {
+import java.util.Scanner;
 
+public class Exercicio005 {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Entre com um horário com número inteiro: ");
+		int x = Integer.parseInt(entrada.nextLine());
+		
+		if (x < 12) {
+			System.out.println("Bom dia!");
+		}
+		else if (x <= 18) {
+			System.out.println("Boa tarde!");
+		}
+		else if (x <= 23) {
+			System.out.println("Boa noite!");
+		}
+		else {
+			System.out.printf("[Error]: não existe o horário: %d", x);
+		}
+		
+		entrada.close();
+	}
 }
